889a2a2d96322838907e978cc8fee7e46c513673
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-04-02 18:42:17 -0400

    Form Input
    Implemented basic user form and started checking input and submission for validation.
    
    Display basic error state for wrong inputs.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,49 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const nameInputRef = useRef();
+
+  const nameInputChangerHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = nameInputRef.current.value;
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangerHandler}
+        />
       </div>
+      {nameInputIsInvalid && (
+        <p style={{ color: "red" }}>Name must not be empty!</p>
+      )}
       <div className="form-actions">
         <button>Submit</button>
       </div>
